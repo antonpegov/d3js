eabd25260be146ef29e5ea537c67699e66d62398
Anton Pegov <antonpegov@gmail.com>
2017-08-29 15:24:50 +0200

    Sample1 "Histogram from tsv" added.

--- d3_app.js
+++ d3_app.js
@@ -1,8 +1,7 @@
-let svg;
 
 // Sample#1 - simple SVG Circle
 import {_SETUP1} from './modules/sample1.js';
-svg = d3.select('.sample1').append('svg').style('overflow', 'visible')
+let svg = d3.select('.sample1').append('svg').style('overflow', 'visible')
 svg.attr('width', _SETUP1.width).attr('height', _SETUP1.height);
 svg.append('circle')
     .attr('r', _SETUP1.scales.r(1380000000))
@@ -13,3 +12,30 @@ svg.append('circle')
 
 // Sample#2 - Simple Histogram
 import {_SETUP2} from './modules/sample2.js';
+
+let bars;       // Массив столбцов гистограмы
+let draw;       // Функция отрисовки гистограмы
+let toNumber;   // Функция подготовки данных - преобразует string  в number
+let chart = d3.select('.sample2').append('svg').attr('width', _SETUP2.width);
+
+draw = (data) => {
+    _SETUP2.scale.domain([0, d3.max(data, (d) => +d.value)]); // выставить границы области данных
+    chart.attr('height', _SETUP2.height * data.length); // выставить высоту svg-контейнера         
+    bars = chart.selectAll('g') // собираем все group tags - аналог div в html
+        .data(data)     // цепляем массив данных к массиву элементов d3
+            .enter()    // собираем массив данных, которым не хватило элементов 'g'
+        .append('g')    // добавляем отсутствующие элементы 'g' на страницу
+            .attr('transform', (d,i) => 'translate(0,'+ i*_SETUP2.height+')')
+    bars.append('rect')
+        .attr('width', (d) => _SETUP2.scale(d.value))
+        .attr('height', _SETUP2.height - 1)
+    bars.append('text')
+        .attr('x', (d) => _SETUP2.scale(d.value - 1))
+        .attr('y', +_SETUP2.height/2)
+        .attr('dy', '.4em')
+        .text((d) => d.value)
+}
+
+toNumber = (d) => { d.value = +d.value; return d;}
+
+d3.tsv('./res/data.tsv', toNumber, draw);
--- modules/sample2.js
+++ modules/sample2.js
@@ -1,2 +1,10 @@
-const setup ={}
+const setup = {
+    width: 600,         // ширина бара (макс.)
+    height: 40,         // высота бара
+    scale:  undefined   // масштаб по x
+}
+setup.scale = d3.scaleLinear()
+    .range([0,setup.width])
+    // domain инициируем после получения данных
+
 export {setup as _SETUP2} ;
