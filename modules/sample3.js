667211e292416ff93c400a0919fd048c49c4b0bd
Anton Pegov <antonpegov@gmail.com>
2017-09-02 13:14:02 +0200

    Some setup for x axis for beter perception

--- modules/sample3.js
+++ modules/sample3.js
@@ -8,8 +8,11 @@ function draw(data) {
     d3.select(el).style('width', width+'px');
     mySVG = dimple.newSvg(el, width, height);
     myChart = new dimple.chart(mySVG, data);
-    xAxis = myChart.addTimeAxis("x", "year"); 
-    yAxis = myChart.addMeasureAxis("y", "attendance");
+    xAxis = myChart.addTimeAxis('x', 'year'); 
+    xAxis.dateParseFormat ='%Y';// Считать по годам
+    xAxis.tickFormat = '%Y';    // Величина шага графика
+    xAxis.timeInterval = 4;     // Шаг для засечек на оси x
+    yAxis = myChart.addMeasureAxis('y', 'attendance');
     myChart.addSeries(null, dimple.plot.bar);
     myChart.draw();
 };
